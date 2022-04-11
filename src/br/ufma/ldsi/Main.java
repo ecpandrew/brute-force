2b235e46532e5d4bdd7dfb0549cc03eb52bd72c9
André Cardoso <andre.cardoso@lsdi.ufma.br>
2022-04-11 17:48:12 -0300

    Topologia estrela

--- src/br/ufma/ldsi/Main.java
+++ src/br/ufma/ldsi/Main.java
@@ -1,13 +1,88 @@
 package br.ufma.ldsi;
 
+import java.util.Arrays;
+
+import static br.ufma.ldsi.Redes.*;
+
 public class Main {
 
 
-
     public static void main(String[] args) {
-        int[] array = new int[]{2, 3,4,5,6,7,8};
-        int number = 2;
-        bruteForce(array, number);
+//        testeBruteForce();
+        testeTopologia(matriz_estrela2);
+        testeTopologia(matriz_estrela);
+
+    }
+
+    private static void testeTopologia(int[][] matriz) {
+
+        if(ehTopologiaAnel(matriz)){
+            System.out.println("Matriz representa topologia Anel");
+        }else if(ehTopologiaEstrela(matriz)){
+            System.out.println("Matriz representa topologia Estrela");
+        }else if(ehTopologiaMalha(matriz)){
+            System.out.println("Matriz representa topologia Malha");
+        }else{
+            System.out.println("Matriz representa outra topologia");
+        }
+
+    }
+
+    private static boolean ehTopologiaMalha(int[][] matriz) { // n²
+        boolean topologiaMalha = true;
+
+        for (int i = 0; i < matriz.length ; i++) { // n
+            for (int j = 0; j < matriz[i].length ; j++) { // n
+                if (matriz[i][j] != 1) { // 1
+                    if(i!=j){ //1
+                        topologiaMalha = false;
+                    }
+                }
+            }
+        }
+        return topologiaMalha;
+    }
+
+    private static boolean ehTopologiaEstrela(int[][] matriz) {// n³ + n² = O(n³)
+        boolean topologiaEstrela = true;
+
+        Integer linha_do_elemento_central = null;
+        Integer indice_do_elemento_central = null;
+        for (int i = 0; i < matriz.length ; i++) { // n
+            int[] linha = matriz[i];
+            if(count(linha, 0) == 1){ // n
+                if(linha_do_elemento_central == null){
+                    linha_do_elemento_central = i;
+                    indice_do_elemento_central = findIndex(linha, 0); // n
+                }else{
+                    return false;
+                }
+            }
+        }
+        if(indice_do_elemento_central==null ){
+            return false;
+        }
+
+        int[] linha = new int[matriz.length];
+
+        for (int i = 0; i < matriz.length ; i++) {
+            linha[i] = matriz[i][indice_do_elemento_central];
+            if(i != linha_do_elemento_central && count(matriz[i],0) != matriz.length-1){
+                return false;
+            }
+
+        }
+        return count(linha,1) == matriz.length-1;
+
+    }
+
+    private static boolean ehTopologiaAnel(int[][] matriz) {
+        boolean topologiaAnel = false;
+        // cada linha da matriz, so pode ter 2 elementos true para a topologia anel
+
+
+        return topologiaAnel;
+
     }
 
     static public void bruteForce( int[] array, int number ){
@@ -22,4 +97,27 @@ public class Main {
         }
     } // Complexidade: N²
      //  ou 2N+3
+
+
+    static void testeBruteForce(){
+        int[] array = new int[]{2, 3,4,5,6,7,8};
+        int number = 2;
+        bruteForce(array, number);
+    }
+
+    static public int count(int [] array, int number) {
+        int c = 0;
+
+        for(int el: array) {
+            if(el == number) c++;
+        }
+        return c;
+    }
+    static public Integer findIndex(int [] array, int element) {
+
+        for (int i = 0; i < array.length ; i++) {
+            if(array[i] == element) return i;
+        }
+        return null;
+    }
 }
---
+++ src/br/ufma/ldsi/Redes.java
@@ -0,0 +1,50 @@
+package br.ufma.ldsi;
+
+public class Redes {
+
+
+    static int linha1_malha[] = new int[] {0, 1, 1, 1, 1, 1};
+    static int linha2_malha[] = new int[] {1, 0, 1, 1, 1, 1};
+    static int linha3_malha[] = new int[] {1, 1, 0, 1, 1, 1};
+    static int linha4_malha[] = new int[] {1, 1, 1, 0, 1, 1};
+    static int linha5_malha[] = new int[] {1, 1, 1, 1, 0, 1};
+    static int linha6_malha[] = new int[] {1, 1, 1, 1, 1, 0};
+
+//            [1, 1, 1, 0, 1, 1]
+//            [0, 0, 0, 1, 0, 0]
+//            [0, 0, 0, 1, 0, 0]
+//            [0, 0, 0, 1, 0, 0]
+//            [0, 0, 0, 1, 0, 0]
+//            [0, 0, 0, 1, 0, 0]
+
+
+                                //           a  b  c  d  e  f
+    static int linha1_estrela2[] = new int[] {0, 0, 1, 0, 0, 0};
+    static int linha2_estrela2[] = new int[] {0, 0, 1, 0, 0, 0};
+    static int linha3_estrela2[] = new int[] {1, 1, 0, 1, 1, 1};
+    static int linha4_estrela2[] = new int[] {0, 0, 1, 0, 0, 0};
+    static int linha5_estrela2[] = new int[] {0, 0, 1, 0, 0, 0};
+    static int linha6_estrela2[] = new int[] {0, 0, 1, 0, 0, 0};
+
+
+
+    static int linha1_estrela[] = new int[] {1, 1, 1, 0, 1, 1};
+    static int linha2_estrela[] = new int[] {0, 0, 0, 1, 0, 0};
+    static int linha3_estrela[] = new int[] {0, 0, 0, 1, 0, 0};
+    static int linha4_estrela[] = new int[] {0, 0, 0, 1, 0, 0};
+    static int linha5_estrela[] = new int[] {0, 0, 0, 1, 0, 0};
+    static int linha6_estrela[] = new int[] {0, 0, 0, 1, 0, 0};
+
+
+    static int[][] matriz_malha_completa = new int[][]{
+            linha1_malha, linha2_malha, linha3_malha, linha4_malha, linha5_malha, linha6_malha};
+
+    static int[][] matriz_estrela = new int[][]{
+            linha1_estrela, linha2_estrela, linha3_estrela, linha4_estrela,   linha5_estrela, linha6_estrela};
+
+    static int[][] matriz_estrela2 = new int[][]{
+            linha1_estrela2, linha2_estrela2, linha3_estrela2,  linha4_estrela2,   linha5_estrela2, linha6_estrela2};
+
+
+
+}
